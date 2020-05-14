a9bd01b70f74dc33ed13bbbbb4f7e1e454425787
master <kar.mateusz@wp.pl>
2020-05-14 10:54:25 +0200

    Wyświtlanie miejsc z bazy na mapie

--- ViewsModels/HomeViewsModel.cs
+++ ViewsModels/HomeViewsModel.cs
@@ -14,5 +14,7 @@ namespace BankZdjecOlsztyn.ViewsModels
         public List<Zdjecie> Zdjecia { get; set; }
         public List<MiejsceTag> MiejscaTagi { get; set; }
         public List<Tag> Tagi { get; set; }
+        public int I_Net { get; set; }
+       
     }
 }
